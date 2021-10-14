390acfda0a06a276139ff7dd18d2e1d81e24d8d1
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2021-10-14 23:45:14 +0300

    changing page names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {ProfileComponent} from "./profile/profile.component";
-import {BoardAdminComponent} from "./board-admin/board-admin.component";
+import {RoleManagementComponent} from "./role-management/role-management.component";
 import {BoardUserComponent} from "./board-user/board-user.component";
 import {BoardOperatorComponent} from "./board-operator/board-operator.component";
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'profile', component: ProfileComponent},
-  {path: 'admin', component: BoardAdminComponent},
+  {path: 'admin', component: RoleManagementComponent},
   {path: 'operator', component: BoardOperatorComponent},
   {path: 'user', component: BoardUserComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'}
+  // {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { RoleManagementComponent } from './role-management/role-management.component';
 import { BoardOperatorComponent } from './board-operator/board-operator.component';
 import { BoardUserComponent } from './board-user/board-user.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { RouterModule } from '@angular/router';
 
-import {authInterceptorProviders} from './_helpers/auth.interceptor'
+import {authInterceptorProviders} from './_helpers/auth.interceptor';
+import { UserComponent } from './user/user.component';
+import { RoleComponent } from './role/role.component'
 
 @NgModule({
   declarations: [
@@ -23,9 +25,9 @@ import {authInterceptorProviders} from './_helpers/auth.interceptor'
     RegisterComponent,
     HomeComponent,
     ProfileComponent,
-    BoardAdminComponent,
+    RoleManagementComponent,
     BoardOperatorComponent,
-    BoardUserComponent
+    BoardUserComponent,
   ],
   imports: [
     BrowserModule,
